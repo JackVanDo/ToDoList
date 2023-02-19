8cc6b2ddb7abc9170ef60af8ea61a2ca483ea9e9
Денисов Евгений <jeton4egg@gmail.com>
2023-02-19 21:29:10 +0300

    Добавьте файлы проекта.

---
+++ ToDoList.Domain/Enum/Priority.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Enum
+
+    /// Описание:
+    /// Приоритет задачи
+    /// 
+
+
+{
+    public enum Priority
+    {
+        [Display(Name = "Простая")]
+        Easy = 0,
+        [Display(Name = "Важная")]
+        Medium = 1,
+        [Display(Name = "Критичная")]
+        High = 2
+    }
+}
